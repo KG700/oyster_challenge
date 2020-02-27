4cb39f0381385e46c95066884244851b0517bb8f
Student <student@makerss-air.home>
2020-02-27 22:53:51 +0000

    slight refactor

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,6 @@
 class Journey
 
-  attr_reader :journeys
+  attr_reader :journeys, :fare
 
   MINIMUM_FARE = 1
   PENALTY_FARE = 6
@@ -13,7 +13,6 @@ class Journey
 
   def add_entry_station entry_station
     @fare += PENALTY_FARE if in_journey?
-    #self.add_journey nil
     @entry_station = entry_station
   end
 
@@ -21,12 +20,6 @@ class Journey
     @journeys.push({entry: @entry_station, exit: exit_station})
     in_journey? ? @fare += MINIMUM_FARE : @fare += PENALTY_FARE
     @entry_station = nil
-  end
-
-  def fare
-    @fare
-    # Needs to return a penalty fare if there is not entry_station or no exit station
-    #in_journey? ? MINIMUM_FARE : PENALTY_FARE
   end
 
   def fare_paid
